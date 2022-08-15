39cd6bc58e5861d0e15dfeab0415b51190c0b2cc
Danilo Trovao <danilotrdev@gmail.com>
2022-08-14 22:51:08 -0300

    Maior

--- QUST_CONDICAOeLACOS/Maior.c
+++ QUST_CONDICAOeLACOS/Maior.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 int main(){
     int a,b,c,maior;
-
-    scanf("%d%d%d", &a,&b,&c);
+    printf("digite 3 valores:\n");
+    scanf("%d %d %d", &a,&b,&c);
 
     maior = a;
 
-    if(b>a){
-        maior = b;}
+    if(b>a && b>c){
+      maior = b;}
     else if(c>a){
-        maior = c;}
+      maior = c;}
 
     printf("o maior valor é: %d", maior);
 
