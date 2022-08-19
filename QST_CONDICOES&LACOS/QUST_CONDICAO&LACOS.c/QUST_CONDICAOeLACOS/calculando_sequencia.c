c69327acc1e482f4f509ace7c56640f13eb0b758
Danilo Trovao <danilotrdev@gmail.com>
2022-08-19 00:50:58 -0300

    cond&lacos

---
+++ QST_CONDICOES&LACOS/QUST_CONDICAO&LACOS.c/QUST_CONDICAOeLACOS/calculando_sequencia.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main(){
+    /*Faça um programa para calcular a seguinte sequência:
+    1) 1 − 2 + 3 − 4 + 5...*/
+    
+    int i=0, controle = 0, result = 0, num=0, cond;
+    while(cond != 0){
+        printf("digite um valor: ");
+        scanf("%d", &num);
+        
+        while(i < num){
+            if(controle == 0){
+                i++;
+                result+=i;
+                controle=1;
+            }
+            else{
+                i++;
+                result-=i;
+                controle=0;
+              }
+            }
+      
+        printf("o resultado da sequencia é: %d", result);
+        printf("\npara continuar, digite 1, para fechar 0\n-> ");
+        scanf("%d", &cond);
+        if(cond == 1){
+            continue;}
+}
+  }
